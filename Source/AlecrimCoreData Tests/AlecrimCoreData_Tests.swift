d7d44ccfe83de8ff76c8c42cc7cb118c6155431a
skyylex <yury.lapitsky@gmail.com>
2015-07-23 00:45:56 +0300

    Initial setup for test target

---
+++ Source/AlecrimCoreData Tests/AlecrimCoreData_Tests.swift
@@ -0,0 +1,18 @@
+//
+//  AlecrimCoreData_Tests.swift
+//  AlecrimCoreData Tests
+//
+//  Created by Yury Lapitsky on 23.07.15.
+//  Copyright (c) 2015 Alecrim. All rights reserved.
+//
+
+import Nimble
+import Quick
+
+class StackSpec: QuickSpec {
+    override func spec() {
+        describe("StackSpec") {
+            
+        }
+    }
+}
